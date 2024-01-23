b921469842f88524a01665228212d42ddb64e89c
shajib126 <shajib126@gmail.com>
2024-01-23 19:30:24 +0600

    update

--- src/pages/Statistic.jsx
+++ src/pages/Statistic.jsx
@@ -4,27 +4,27 @@ import donatedata from '../data.json'
 
 //statistic page
 // const totalDonation = data.length
-const donated = JSON.parse(localStorage.getItem('donation'))
-let totalPrice =0
- donatedata.forEach((item)=>{
-  totalPrice += item.price
-})
-let totalDonated = 0 
-donated.forEach((item)=>{
-  totalDonated += item.price
-}) 
-const data = [
-    ["Donation", "Donation Stats"],
-    ["Total Donation", totalPrice],
-    ["Your Donation", totalDonated],
-  ];
+
 export const options = {
     title: "Donation",
   };
 
  
 const Statistic = () => {
-    
+  const donated = JSON.parse(localStorage.getItem('donation'))
+  let totalPrice =0
+   donatedata.forEach((item)=>{
+    totalPrice += item.price
+  })
+  let totalDonated = 0 
+  donated.forEach((item)=>{
+    totalDonated += item.price
+  }) 
+  const data = [
+      ["Donation", "Donation Stats"],
+      ["Total Donation", totalPrice],
+      ["Your Donation", totalDonated],
+    ];
   
   return (
     <div>
